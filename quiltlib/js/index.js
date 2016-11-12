3358d9b222d9ce67e32c26d76d7ae305ea76e9bd
Ryan Marcus <ryan@rmarcus.info>
2016-11-12 15:04:01 -0500

    added new methods

--- quiltlib/js/index.js
+++ quiltlib/js/index.js
@@ -10,39 +10,55 @@ const texture = {
 
 console.log(Object.keys(texture));
 
+function transfer(textureData, imgData, alpha, tileSize, overlap) {
+    const d = textureData;
+    const corro = imgData;
 
-function transfer(textureURL, imgURL, alpha, tileSize, overlap) {
+    const width = corro.width;
+    const height = corro.height;
+    
+    const dSize = width * height * 4;
+    const dPtr = texture._malloc(dSize);
+    const dView = new Uint8ClampedArray(texture.HEAPU8.buffer, dPtr, dSize);
+    
+    const sSize = d.width * d.height * 4;
+    const sPtr = texture._malloc(sSize);
+    const sView = new Uint8Array(texture.HEAPU8.buffer, sPtr, sSize);
+
+    const cSize = width * height * 4;
+    const cPtr = texture._malloc(cSize);
+    const cView = new Uint8ClampedArray(texture.HEAPU8.buffer, cPtr, cSize);
+    
+    // copy data from d.data to oView
+    sView.set(d.data);
+
+    // copy data from corro.data to cView
+    cView.set(corro.data);
+
+    texture._doTransfer(d.width, d.height, width, height, sPtr, dPtr,
+                        cPtr, alpha, tileSize, overlap);
+
+    return new ImageData(dView, width, height);
+}
+
+function transferFromCanvas(textureURL, imgCanvas, alpha, tileSize, overlap) {
+    return image_utils.getImage(textureURL)
+        .then((d) => {
+            const ctx = imgCanvas.getContext("2d");
+            const corro = ctx.getImageData(0, 0,
+                                           imgCanvas.width,
+                                           imgCanvas.height);
+            return transfer(d, corro, alpha, tileSize, overlap);
+            
+        });
+}
+
+function transferFromURL(textureURL, imgURL, alpha, tileSize, overlap) {
     return q.all([image_utils.getImage(textureURL),
                   image_utils.getImage(imgURL)])
         .then((items) => {
-            const d = items[0];
-            const corro = items[1];
-
-            const width = corro.width;
-            const height = corro.height;
-            
-            const dSize = width * height * 4;
-            const dPtr = texture._malloc(dSize);
-            const dView = new Uint8ClampedArray(texture.HEAPU8.buffer, dPtr, dSize);
-            
-            const sSize = d.width * d.height * 4;
-            const sPtr = texture._malloc(sSize);
-            const sView = new Uint8Array(texture.HEAPU8.buffer, sPtr, sSize);
-
-            const cSize = width * height * 4;
-            const cPtr = texture._malloc(cSize);
-            const cView = new Uint8ClampedArray(texture.HEAPU8.buffer, cPtr, cSize);
-            
-            // copy data from d.data to oView
-            sView.set(d.data);
-
-            // copy data from corro.data to cView
-            cView.set(corro.data);
-
-            texture._doTransfer(d.width, d.height, width, height, sPtr, dPtr,
-                                cPtr, alpha, tileSize, overlap);
-
-            return new ImageData(dView, width, height);
+            return transfer(items[0], items[1],
+                            alpha, tileSize, overlap);
         });
 }
 
@@ -70,6 +86,7 @@ function synth() {
 }
 
 
-window.transfer = transfer;
+window.transferFromURL = transferFromURL;
+window.transferFromCanvas = transferFromCanvas;
 
 
